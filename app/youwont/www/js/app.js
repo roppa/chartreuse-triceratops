3fd74f67f35e24d266cb2a02e011bae7cf36830b
Surya kotamraju <srtejk@gmail.com>
2015-08-06 12:18:06 -0700

    added header, grid and footer to homepage

--- app/youwont/www/js/app.js
+++ app/youwont/www/js/app.js
@@ -3,17 +3,32 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var ionicApp = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+ionicApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+// gridController to set the initial page display
+ionicApp.controller("gridController", function($scope) {
+  // $scope.data = {};
+  $scope.data = [];
+  $scope.showData = function() {
+    for (var i = 0; i < 20; i++) {
+      $scope.data.push({
+        id: i,
+        src: "http://placehold.it/50x50",
+        text: "Some random text"
+      });
+    }
+  };
+});
