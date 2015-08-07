25083d2235aac42c342aad8cc705f1d11f589103
Jonathan Schapiro <jschapir@gmail.com>
2015-08-07 13:08:40 -0700

    login states - infinite loop

--- app/youwont/www/js/app.js
+++ app/youwont/www/js/app.js
@@ -1,4 +1,4 @@
-var youwont = angular.module('youwont', ['ionic', 'youwont.controllers', 'youwont.factory'])
+var youwont = angular.module('youwont', ['ionic', 'youwont.controllers', 'youwont.factory','FacebookLogin'])
 
 
 youwont.config(function ($stateProvider, $urlRouterProvider) {
@@ -29,7 +29,7 @@ youwont.config(function ($stateProvider, $urlRouterProvider) {
 
 });
 
-youwont.run(function($ionicPlatform) {
+youwont.run(function($ionicPlatform,$rootScope,authLogin,$state) {
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -46,6 +46,17 @@ youwont.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
+
+  $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+       
+        if (toState.name !== '/login' && !authLogin.checkState()){
+           $state.go('login')
+          event.preventDefault();
+        }
+        
+       
+
+    });
 });
 
 
--- app/youwont/www/js/factory.js
+++ app/youwont/www/js/factory.js
@@ -2,7 +2,7 @@ var youwontFactory = angular.module('youwont.factory', []);
 
 //Facebook OAuth Login Factory
 var facebookLoginFactory = angular.module('FacebookLogin',[]);
-facebookLoginFactory.factory('authLogin',function(){
+facebookLoginFactory.factory('authLogin',function($state){
 	//URL to the sayiwont Firebase DB
 	var appURL = "https://sayiwont.firebaseio.com";
 	var login = {};
@@ -11,17 +11,48 @@ facebookLoginFactory.factory('authLogin',function(){
 
 	//function to perform OAuth login with FB
 		
+
+	login.logUserIn = function(){
 		login.ref.authWithOAuthPopup("facebook", function(error, authData) {
-			console.log('hello world')
 		  if (error) {
 		    console.log("Login Failed!", error);
 		  } else {
+		  	//user is successfully logged in and routed to the home page
 		    console.log("Authenticated successfully with payload:", authData);
+		  	
+		  	$state.go('home')
 		  }
 		});
+	}
 	
+	login.logout = function(){
+		//routes to the login page and unauthorizes the user
+		
+		login.ref.unauth();
+		$state.go('login')
+		
+	}
 
-	console.log('we in here')
+	login.checkState = function(){
+		//get auth status
+		console.log('checking state')
+		var isLoggedIn = false;
+		var authData = login.ref.getAuth();
+		
+		if (authData){
+			console.log('User is logged in!')
+
+			isLoggedIn = true;
+
+		} else {
+
+			isLoggedIn = false;
+			
+		}
+		
+		return isLoggedIn
+	};
+	
 
 	return login;
 });
